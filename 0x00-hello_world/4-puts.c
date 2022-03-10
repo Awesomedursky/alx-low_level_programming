bd4c2b69cdf60608beff3b7bb17a24f48a470f15
Awesomedursky <awesomedursky@gmail.com>
2022-03-10 14:14:03 -0800

    D

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,5 @@
-#include <studio.h>
-
-/**
-*main - pprints the phrase "Programming is like building a multilingual puzzle"
-*Return: 0 if exited properly, non-zero otherwise
-*/
-int main(void)
-{
+#include <stdio.h>
+int main(void){
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
